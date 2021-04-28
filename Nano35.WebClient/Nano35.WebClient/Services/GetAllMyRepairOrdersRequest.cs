6e725f1110473afab5f2d36796ebbb97ccb03d76
CandyChips <Crazy@killer-b.ru>
2021-04-28 21:33:48 +0300

    Some default fixes

---
+++ Nano35.WebClient/Nano35.WebClient/Services/GetAllMyRepairOrdersRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Nano35.HttpContext.instance;
+using Nano35.HttpContext.Repair;
+
+namespace Nano35.WebClient.Services
+{
+    public class GetAllMyRepairOrdersRequest : 
+        RequestProvider<GetAllMyRepairOrdersBody, GetAllMyRepairOrdersSuccessResponse>
+    {
+        public GetAllMyRepairOrdersRequest(IRequestManager requestManager, HttpClient httpClient, GetAllMyRepairOrdersBody request) : 
+            base(requestManager, httpClient, request)
+        {
+        }
+
+        public override async Task<GetAllMyRepairOrdersSuccessResponse> Send()
+        {
+            var response = await HttpClient.GetAsync($"{RequestManager.LocalRepairOrdersServer}/RepairOrders/My");
+            if (response.IsSuccessStatusCode)
+            {
+                return (await response.Content.ReadFromJsonAsync<GetAllMyRepairOrdersSuccessResponse>());
+            }
+            throw new Exception((await response.Content.ReadFromJsonAsync<string>()));
+        }
+    }
+}

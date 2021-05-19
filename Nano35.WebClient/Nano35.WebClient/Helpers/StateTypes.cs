f9464ef38eaa46b9b760faae9d1889f234047035
CandyChips <Crazy@killer-b.ru>
2021-05-19 22:01:46 +0300

    Some major fixes

--- Nano35.WebClient/Nano35.WebClient/Helpers/StateTypes.cs
+++ Nano35.WebClient/Nano35.WebClient/Helpers/StateTypes.cs
@@ -10,4 +10,28 @@
         PerformerChanged = 5,
         Closed = 6
     }
+
+    public class RepairOrderStateTypesHelper
+    {
+        public static int Created => 0;
+        public static int InWork => 1;
+        public static int Completed => 2;
+        public static int Stopped => 3;
+        public static int OnWait => 4;
+        public static int PerformerChanged => 5;
+        public static int Closed => 6;
+        
+        public static string GetName(int id)
+        {
+            return 
+                id == Created ? "Заказ на ремонт создан" :
+                id == InWork ? "Заказ на ремонт в работе" :
+                id == Completed ? "Заказ на ремонт выполнен" :
+                id == OnWait ? "Заказ на ремонт ожидает запчасти" :
+                id == PerformerChanged ? "Изменен исполнитель" :
+                id == Closed ? "Заказ на ремонт закрыт" :
+                id == Stopped ? "Заказ на ремонт приостановлен" : 
+                "";
+        }
+    }
 }
--- Nano35.WebClient/Nano35.WebClient/Services/RequestProvider.cs
+++ Nano35.WebClient/Nano35.WebClient/Services/RequestProvider.cs
@@ -1,9 +1,11 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Nano35.HttpContext.storage;
 
 namespace Nano35.WebClient.Services
 {
-    
     public abstract class RequestProvider<TReq, TSRes>
     {
         protected readonly HttpClient HttpClient;
@@ -18,5 +20,27 @@ namespace Nano35.WebClient.Services
         }
 
         public abstract Task<TSRes> Send();
+    }
+
+    public class GetRequestProvider<TReq, TSRes> : RequestProvider<TReq, TSRes>
+    {
+        public GetRequestProvider(HttpClient httpClient, TReq request) : base(null, httpClient, request) { }
+        private string _addr;
+        
+        public async Task<TSRes> Ask(string addr)
+        {
+            _addr = addr;
+            return await Send();
+        }
+        
+        public override async Task<TSRes> Send()
+        {
+            var response = await HttpClient.GetAsync(_addr);
+            if (response.IsSuccessStatusCode)
+            {
+                return (await response.Content.ReadFromJsonAsync<TSRes>());
+            }
+            throw new Exception((await response.Content.ReadFromJsonAsync<string>()));
+        }
     }
 }

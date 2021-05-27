6b378208ae8494c991103bfd838fa83eeb680272
Teilat <kot.sergej2012@yandex.ru>
2021-05-27 20:36:25 +0300

    move from separated requests

--- Nano35.WebClient/Nano35.WebClient/Services/HttpRequestWrapper.cs
+++ Nano35.WebClient/Nano35.WebClient/Services/HttpRequestWrapper.cs
@@ -46,48 +46,47 @@ namespace Nano35.WebClient.Services
             else throw new Exception(await response.Content.ReadFromJsonAsync<string>());
         }
     }
-        
-    public class HttpRequestWrapper<TRequest, TSuccess>
-        where TRequest : IHttpRequest
-        where TSuccess : IHttpResponse
+    
+    public class HttpPatchRequest<TBody, TResponse>
+        where TBody : IHttpRequest
+        where TResponse : IHttpResponse
     {
         private readonly HttpClient _httpClient;
-        private readonly string _addr;
-
-        public HttpRequestWrapper(HttpClient httpClient, string addr)
+        private readonly string _uri;
+        public HttpPatchRequest(HttpClient httpClient, string uri)
         {
             _httpClient = httpClient;
-            _addr = addr;
+            _uri = uri;
         }
-
-        public async Task<TSuccess> GetAsync()
-        {
-            var response = await _httpClient.GetAsync(_addr);
-            if (response.IsSuccessStatusCode)
-            {
-                return (await response.Content.ReadFromJsonAsync<TSuccess>());
-            }
-            throw new Exception(await response.Content.ReadFromJsonAsync<string>());
-        }
-        public async Task<TSuccess> PostAsync(TRequest request)
+        public async Task<TResponse> PatchAsync(TBody request)
         {
             var req = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
-            var response = await _httpClient.PostAsync(_addr, req);
-            if (response.IsSuccessStatusCode)
-            {
-                return (await response.Content.ReadFromJsonAsync<TSuccess>());
-            }
-            throw new Exception(await response.Content.ReadFromJsonAsync<string>());
-        }
-        public async Task<TSuccess> PatchAsync(TRequest request)
-        {
-            var req = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
-            var response = await _httpClient.PatchAsync(_addr, req);
-            if (response.IsSuccessStatusCode)
-            {
-                return (await response.Content.ReadFromJsonAsync<TSuccess>());
-            }
-            throw new Exception(await response.Content.ReadFromJsonAsync<string>());
+            var response = await _httpClient.PatchAsync(_uri, req);
+            if (response.IsSuccessStatusCode) return await response.Content.ReadFromJsonAsync<TResponse>();
+            else throw new Exception(await response.Content.ReadFromJsonAsync<string>());
         }
     }
+    
+    public class HttpDeleteRequest< TResponse>
+        where TResponse : IHttpResponse
+        {
+            private readonly HttpClient _httpClient;
+            private readonly string _uri;
+            public HttpDeleteRequest(HttpClient httpClient, string uri)
+            {
+                _httpClient = httpClient;
+                _uri = uri;
+            }
+            public async Task<TResponse> DeleteAsync()
+            {
+                var response = await _httpClient.DeleteAsync(_uri);
+                if (response.IsSuccessStatusCode) return await response.Content.ReadFromJsonAsync<TResponse>();
+                else throw new Exception(await response.Content.ReadFromJsonAsync<string>());
+            }
+        }
 }
+
+// (await new HttpGetRequest<GetInstanceByIdHttpResponse>(HttpClient, $"{RequestManager.}/Instances/{id}").GetAsync()).Data;
+// await new HttpPostRequest<CreateArticleHttpBody,CreateArticleSuccessHttpResponse>(HttpClient, $"{RequestManager.}/Articles").PostAsync(_model);
+// await new HttpPatchRequest<UpdateArticleInfoHttpBody, UpdateArticleInfoSuccessHttpResponse>(HttpClient, $"{RequestManager.}/Articles/Info").PatchAsync(InfoEdit)
+// await new HttpDeleteRequest<DeleteClientSuccessHttpResponse>(HttpClient, $"{RequestManager.}/Clients/{Client.Id}").DeleteAsync();
